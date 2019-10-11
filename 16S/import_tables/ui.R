3960d82c309f699ea8bb4a426db3844afb932546
yukicyk <yukichanyk@gmail.com>
2019-10-11 13:02:27 +0800

    Update ui.R

--- 16S/import_tables/ui.R
+++ 16S/import_tables/ui.R
@@ -18,12 +18,12 @@ shinyUI(
     fluidRow(
       wellPanel(
         class = "desc-box",
-        h5("Imported tables was used to construct the phyloseq object and store in the genepiper."),
-        h5(tags$span(class = "bold", "All tables"), "must not contain row names and row numbers."),
-        h5(tags$span(class = "bold", "OTU table"), "must be provided and should contains only one character column and without 'NA' value. The first column should be the OTU name column."),
-        h5(tags$span(class = "bold", "Taxonomy table"), "must be provided and should contains all character columns. The first column should be OTU name and each taxonomical rank in individual column."),
-        h5(tags$span(class = "bold", "Sample data"), "must be provided and should contains at least two columns. The first column should be the sample name."),
-        h5(tags$span(class = "bold", "Phylogenetic tree"), "is optional but highly recommended. It was used for calculating 'Unifrac' distance and taxonomical tree analysis. A helper function was provided to build the ML tree by OTU representative sequences.")
+        h5("Imported tables will be used to construct a phyloseq-class object and stored in the GenePiper environment. This file could be exported in the Download Phyloseq RDS panel."),
+        h5(tags$span(class = "bold", "All tables"), "should be in either tab- or comma-delimited text format. See SampleData for examples of these table format"),
+        h5(tags$span(class = "bold", "OTU table"), "must be provided. Abundance matrix of taxa (rows) by samples (columns).The first column should contain the OTU/ASV/species/gene names (characters);  first row should contain sample names (characters). Absolute number of the sequencing reads in the matrix without 'NA' value."),
+        h5(tags$span(class = "bold", "Taxonomy table"), "must be provided and should contain all character columns. The first column should match that of the OTU table."),
+        h5(tags$span(class = "bold", "Sample data"), "must be provided and should contain at least two columns. The first column should be the sample names."),
+        h5(tags$span(class = "bold", "Phylogenetic tree"), "should be either in Newick or Nexus format; is optional but highly recommended. It will be used for calculation of the 'Unifrac' distance (Lozupone & Knight 2005) and taxonomical tree analysis. A helper function is available in the main page to build a Maximum Likelihood tree from representative sequences provided in fasta format.")
       ),
       
       column(
@@ -37,7 +37,7 @@ shinyUI(
                                         placement = "left",
                                         html = "true",
                                         content = "<UL>
-                                        <LI>no row names and row numbers
+                                        <LI>no row names or row numbers
                                         <LI>the first column contains OTU names
                                         <LI>no 'NA' value
                                         </UL>")),
@@ -61,7 +61,7 @@ shinyUI(
                                         content = "<UL>
                                         <LI>no row names and row numbers
                                         <LI>the first column contains OTU names
-                                        <LI>all columns are character>
+                                        <LI>all columns are characters
                                         <LI>each taxonomical rank in individual column
                                         </UL>")),
             h4("Taxonomy Table File:")
@@ -103,8 +103,8 @@ shinyUI(
                        bs_embed_popover(title = "Phylogenetic Tree",
                                         placement = "left",
                                         html = "true",
-                                        content = tags$div(tags$p("Phylogenetic Tree was used for 'Unifrac' distance calculation and taxonomical tree analysis."),
-                                                           tags$p("A helper function was provided to build the ML tree by OTU representative sequences.")))),
+                                        content = tags$div(tags$p("Phylogenetic Tree will be used for Unifrac distance calculation and taxonomical tree analysis."),
+                                                           tags$p("A helper function is available in the main page to build the ML tree from representative sequences.")))),
             h4("Phylogenetic Tree File (Optional):")
           ),
           fileInput("treeFilePath", "File Path (Newick or Nexus format)"),
@@ -133,8 +133,8 @@ shinyUI(
                        bs_embed_popover(title = "Project",
                                         placement = "left",
                                         html = "true",
-                                        content = tags$div(tags$p("Project is to group related data for storage."),
-                                                           tags$p("New Project button can help to create a new project title.")))),
+                                        content = tags$div(tags$p("Group related dataset into project."),
+                                                           tags$p("Press New Project button to create and name a new project.")))),
             h4("Project:")
           ),
           selectInput("project", NULL, NULL),
@@ -150,7 +150,7 @@ shinyUI(
                 bs_embed_popover(title = "Data Label",
                                  placement = "left",
                                  html = "true",
-                                 content = "Data label is to name the imported data. The data will be recalled by the data label within Genepiper")
+                                 content = "Data label provides name to the imported data. You may recall the dataset by this data label within GenePiper")
             ),
           errorOutput("saveMessage"),
           actionButton("saveButton", "Save")
@@ -165,42 +165,3 @@ shinyUI(
     )
   )
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
