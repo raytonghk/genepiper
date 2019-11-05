d18cdc0e45dca145fd75c011175cba76f26970e2
raytonghk <raytong@hku.hk>
2019-11-05 15:11:16 +0800

    Add Import Biom Module

---
+++ 16S/import_biom/server.R
@@ -0,0 +1,365 @@
+library(shiny)
+library(tidyverse)
+library(phyloseq)
+library(phangorn)
+library(shinyjs)
+library(biomformat)
+
+options(shiny.maxRequestSize = 100*1024^2)
+
+shinyServer(
+  function(input, output, session) {
+    vals <- reactiveValues()
+    
+    output$showTaxTableInput <- reactive(vals$showTaxTableInput)
+    outputOptions(output, "showTaxTableInput", suspendWhenHidden = FALSE)
+    
+    output$showSampleDataInput <- reactive(vals$showSampleDataInput)
+    outputOptions(output, "showSampleDataInput", suspendWhenHidden = FALSE)
+    
+    output$showPhyloTreeInput <- reactive(vals$showPhyloTreeInput)
+    outputOptions(output, "showPhyloTreeInput", suspendWhenHidden = FALSE)
+    
+    source("../function_import_data.R")
+    
+    ## Biom File
+    observe(
+      {
+        req(input$biomFilePath$datapath)
+        isolate(
+          {
+            vals$biomFilePathMessage <- NULL
+            vals$biom <- NULL
+            vals$otuTable <- NULL
+            vals$taxTable <- NULL
+            vals$samData <- NULL
+            vals$tree <- NULL
+            vals$showTaxTableInput <- FALSE
+            vals$showSampleDataInput <- FALSE
+            vals$showPhyloTreeInput <- FALSE
+            tryCatch(
+              {
+                vals$biom <- read_biom(input$biomFilePath$datapath)
+              },
+              error = function(e) {
+                vals$biomFilePathMessage <- "Import biom file error!"
+              }
+            )
+          }
+        )
+      }
+    )
+    
+    ## OTU Table
+    observe(
+      {
+        req(vals$biom)
+        tryCatch(
+          {
+            vals$otuTable <- as(biom_data(vals$biom), "matrix") %>%
+              as.data.frame() %>%
+              rownames_to_column("taxa")
+            vals$showPhyloTreeInput <- TRUE
+          },
+          errror = function(e) {
+            vals$biomFilePathMessage <- "OTU table error!"
+          }
+        )
+      }
+    )
+    
+    observe(
+      {
+        req(vals$otuTable)
+        vals$otuFilePathMessage <- NULL
+        vals$validOtuTable <- NULL
+        if(validateOtuTable(vals$otuTable)) {
+          vals$validOtuTable <- formatOtuTable(vals$otuTable)
+        } else {
+          vals$biomFilePathMessage <- "Import file format error!"
+        }
+      }
+    )
+    
+    output$biomFilePathMessage <- renderText(HTML(vals$biomFilePathMessage))
+    
+    ### Taxonomy Table
+    observe(
+      {
+        req(vals$biom)
+        if(is.null(observation_metadata(vals$biom))) {
+          vals$showTaxTableInput <- TRUE
+        } else {
+          tryCatch(
+            {
+              vals$taxTable <- observation_metadata(vals$biom) %>%
+                rownames_to_column("taxa")
+            },
+            error = function(e) {
+              vals$bioFilePathMessage <- "Taxa table in biom file error!"
+              vals$showTaxTableInput <- TRUE
+            }
+          )
+        }
+      }
+    )
+    
+    observe(
+      {
+        req(input$taxFilePath$datapath, input$taxFileType)
+        vals$taxFilePathMessage <- NULL
+        vals$taxTable <- NULL
+        isolate(
+          {
+            tryCatch(
+              {
+                vals$taxTable <- switch(input$taxFileType,
+                                        csv = read_csv(input$taxFilePath$datapath, comment = ""),
+                                        tsv = read_tsv(input$taxFilePath$datapath, comment = ""))
+              },
+              error = function(e) {
+                vals$taxFilePathMessage <- "Import file error!"
+              }
+            )
+          }
+        )
+      }
+    )
+    
+    output$taxFilePathMessage <- renderText(HTML(vals$taxFilePathMessage))
+    
+    observe(
+      {
+        req(vals$taxTable)
+        vals$taxFilePathMessage <- NULL
+        vals$validTaxTable <- NULL
+        if(validateTaxTable(vals$taxTable)) {
+          vals$validTaxTable <- formatTaxTable(vals$taxTable)
+        } else {
+          vals$taxFilePathMessage <- "Import file format error!"
+        }
+      }
+    )
+    
+    output$taxDetails <- renderPrint(
+      {
+        req(vals$validTaxTable)
+        taxDetails(vals$taxTable)
+      }
+    )
+    
+    ### Sample Data
+    observe(
+      {
+        req(vals$biom)
+        if(is.null(sample_metadata(vals$biom))) {
+          vals$showSampleDataInput <- TRUE
+        } else {
+          tryCatch(
+            {
+              vals$samData <- sample_metadata(vals$biom) %>%
+                rownames_to_column("sample")
+            },
+            error = function(e) {
+              vals$bioFilePathMessage <- "Sample data in biom file error!"
+              vals$showSampleDataInput <- TRUE
+            }
+          )
+        }
+      }
+    )
+    
+    observe(
+      {
+        req(input$samFilePath$datapath, input$samFileType)
+        vals$samFilePathMessage <- NULL
+        vals$samData <- NULL
+        isolate(
+          {
+            tryCatch(
+              {
+                vals$samData <- switch(input$samFileType,
+                                       csv = read_csv(input$samFilePath$datapath, comment = ""),
+                                       tsv = read_tsv(input$samFilePath$datapath, comment = ""))
+              },
+              error = function(e) {
+                vals$samFilePathMessage <- "Import file error!"
+              }
+            )
+          }
+        )
+      }
+    )
+    
+    output$samFilePathMessage <- renderText(HTML(vals$samFilePathMessage))
+    
+    observe(
+      {
+        req(vals$samData)
+        vals$samFilePathMessage <- NULL
+        vals$validSamData <- NULL
+        if(validateSamData(vals$samData)) {
+          vals$validSamData <- formatSampleData(vals$samData)
+        } else {
+          vals$samFilePathMessage <- "Import file format error!"
+        }
+      }
+    )
+    
+    output$samDetails <- renderPrint(
+      {
+        req(vals$validSamData)
+        sampleDetails(vals$samData)
+      }
+    )
+    
+    ### Phylogenetic Tree File
+    observe(
+      {
+        req(input$treeFilePath$datapath)
+        vals$treeFilePathMessage <- NULL
+        vals$tree <- NULL
+        isolate(
+          {
+            tryCatch(
+              {
+                vals$tree <- phangorn::midpoint(read_tree(input$treeFilePath$datapath)) %>%
+                  formatTree()
+              },
+              error = function(e) {
+                vals$treeFilePathMessage <- "Import file error!"
+              }
+            )
+          }
+        )
+      }
+    )
+    
+    output$treeDetails <- renderPrint(vals$tree)
+    
+    ### Phyloseq
+    observe(
+      {
+        req(vals$validOtuTable, vals$validTaxTable)
+        vals$phyloseqMessage <- NULL
+        if(!all(rownames(vals$validOtuTable) %in% rownames(vals$validTaxTable))) {
+          vals$phyloseqMessage <- "Taxa names are not match between OTU table and taxonomy table."
+        }
+      }
+    )
+    
+    observe(
+      {
+        req(vals$validOtuTable, vals$validSamData)
+        vals$phyloseqMessage <- NULL
+        if(!all(colnames(vals$validOtuTable) %in% rownames(vals$validSamData))) {
+          vals$phyloseqMessage <- "Sample names are not match between OTU table and sample data."
+        }
+      }
+    )
+    
+    observe(
+      {
+        req(vals$validOtuTable, vals$tree)
+        vals$phyloseqMessage <- NULL
+        if(!all(rownames(vals$validOtuTable) %in% vals$tree$tip.label)) {
+          vals$phyloseqMessage <- "Taxa names are not match between OTU table and phylogenetic tree."
+        }
+      }
+    )
+    
+    output$phyloseqMessage <- renderText(HTML(vals$phyloseqMessage))
+    
+    observe(
+      {
+        req(vals$validOtuTable, vals$validTaxTable, vals$validSamData, !isTruthy(vals$tree))
+        vals$phyloseq <- NULL
+        try(
+          {
+            vals$phyloseq <- phyloseq(otu_table(as.matrix(vals$validOtuTable), taxa_are_rows = TRUE),
+                                      tax_table(as.matrix(vals$validTaxTable)),
+                                      sam_data(vals$validSamData))
+          },
+          silent = TRUE 
+        )
+        
+      }
+    )
+    
+    observe(
+      {
+        req(vals$validOtuTable, vals$validTaxTable, vals$validSamData, vals$tree)
+        vals$phyloseq <- NULL
+        try(
+          {
+            vals$phyloseq <- phyloseq(otu_table(as.matrix(vals$validOtuTable), taxa_are_rows = TRUE),
+                                      tax_table(as.matrix(vals$validTaxTable)),
+                                      sam_data(vals$validSamData),
+                                      phy_tree(vals$tree))
+          },
+          silent = TRUE
+        )
+      }
+    )
+    
+    output$phyloseqDetails <- renderPrint(vals$phyloseq)
+    
+    ### Project
+    source("../dialog_project.R", local = TRUE)
+    
+    updateSelectInput(session, "project", choices = projectNames())
+    
+    observeEvent(
+      input$newProjectButton,
+      {
+        showModal(newProjectDialog())
+      }
+    )
+    
+    ### Save
+    observe(
+      {
+        disable("saveButton")
+        req(vals$phyloseq, input$dataLabel)
+        enable("saveButton")
+      }
+    )
+    
+    observeEvent(
+      input$saveButton,
+      {
+        vals$saveMesssage <- NULL
+        path <- getProjectPath(input$project)
+        if(!dir.exists(path)) {
+          system(paste0("mkdir ", path, " -m 777"))
+        }
+        
+        filepath <- paste0(getProjectPath(input$project), "/", input$dataLabel, "_phyloseq.rds")
+        if(file.exists(filepath)) {
+          vals$saveMessage <- "Data label exists!"
+        } else {
+          saveRDS(vals$phyloseq, filepath)
+          vals$saveMessage <- "Saved."
+          disable("saveButton")
+        }
+      }
+    )
+    
+    output$saveMessage <- renderText(HTML(vals$saveMessage))
+    
+    ### Download
+    observe(
+      {
+        disable("downloadButton")
+        req(vals$phyloseq)
+        enable("downloadButton")
+      }
+    )
+    
+    output$downloadPhyloseqButton <- downloadHandler("phyloseq.rds",
+                                                     function(file) {
+                                                       saveRDS(vals$phyloseq, file)
+                                                     }
+    )
+  }
+)
---
+++ 16S/import_biom/ui.R
@@ -0,0 +1,160 @@
+library(shiny)
+library(shinyjs)
+library(bsplus)
+source("../style.R")
+source("../ui_additional.R")
+
+shinyUI(
+  fluidPage(
+    useShinyjs(),
+    theme = "../style.css",
+    tags$script(type = "text/javascript", src = "../www/busy.js"),
+    tags$script(type = "text/javascript", src = "../www/controls.js"),
+    tags$div(class = "busy", p("Busy..."), img(src = "../www/hour-glass.gif")),
+    tags$script(HTML("window.onload = function() { parent.postMessage('importBiomLoaded', '*') }")),
+    eval(parse(text = style())),
+    use_bs_popover(),
+    
+    fluidRow(
+      wellPanel(
+        class = "desc-box",
+        h5("Imported biom file was used to construct the phyloseq object and store in the genepiper."),
+        h5("If following tables were not included in biom file, please supply with table formats:"),
+        h5(tags$span(class = "bold", "Taxonomy table"), "should contains all character columns. The first column should be OTU name and each taxonomical rank in individual column."),
+        h5(tags$span(class = "bold", "Sample data"), "should contains at least two columns. The first column should be the sample name."),
+        h5(tags$span(class = "bold", "Phylogenetic tree"), "is optional but highly recommended. It was used for calculating 'Unifrac' distance and taxonomical tree analysis. A helper function was provided to build the ML tree by OTU representative sequences.")
+      ),
+      
+      column(
+        width = 4,
+        wellPanel(
+          class = "left-column",
+          h4("Biom File:"),
+          fileInput("biomFilePath", "File Path"),
+          errorOutput("biomFilePathMessage")
+        ),
+        
+        conditionalPanel(
+          condition = "output.showTaxTableInput",
+          wellPanel(
+            class = "left-column",
+            tags$div(
+              tags$div(class = "pull-right",
+                       shiny_iconlink() %>%
+                         bs_embed_popover(title = "Taxonomy Table Format",
+                                          placement = "left",
+                                          html = "true",
+                                          content = "<UL>
+                                        <LI>no row names and row numbers
+                                        <LI>the first column contains OTU names
+                                        <LI>all columns are character>
+                                        <LI>each taxonomical rank in individual column
+                                        </UL>")),
+              h4("Taxonomy Table File:")
+            ),
+            selectInput("taxFileType", "File Type", list("Comma-separated file" = "csv",
+                                                         "Tab-separated file" = "tsv")),
+            fileInput("taxFilePath", "File Path"),
+            errorOutput("taxFilePathMessage"),
+            verbatimTextOutput("taxDetails")
+          )
+        ),
+        
+        conditionalPanel(
+          condition = "output.showSampleDataInput",
+          wellPanel(
+            class = "left-column",
+            tags$div(
+              tags$div(class = "pull-right",
+                       shiny_iconlink() %>%
+                         bs_embed_popover(title = "Sample Data Format",
+                                          placement = "left",
+                                          html = "true",
+                                          content = "<UL>
+                                        <LI>no row names and row numbers<br>
+                                        <LI>the first column contains sample names<br>
+                                        <LI>at least two columns
+                                        </UL>")),
+              h4("Sample Data File:")
+            ),
+            selectInput("samFileType", "File Type", list("Comma-separated file" = "csv",
+                                                         "Tab-separated file" = "tsv")),
+            fileInput("samFilePath", "File Path"),
+            errorOutput("samFilePathMessage"),
+            verbatimTextOutput("samDetails")
+          )
+        ),
+        
+        conditionalPanel(
+          condition = "output.showPhyloTreeInput",
+          wellPanel(
+            class = "left-column",
+            tags$div(
+              tags$div(class = "pull-right",
+                       shiny_iconlink() %>%
+                         bs_embed_popover(title = "Phylogenetic Tree",
+                                          placement = "left",
+                                          html = "true",
+                                          content = tags$div(tags$p("Phylogenetic Tree was used for 'Unifrac' distance calculation and taxonomical tree analysis."),
+                                                             tags$p("A helper function was provided to build the ML tree by OTU representative sequences.")))),
+              h4("Phylogenetic Tree File (Optional):")
+            ),
+            fileInput("treeFilePath", "File Path (Newick or Nexus format)"),
+            errorOutput("treeFilePathMessage"),
+            verbatimTextOutput("treeDetails")
+          )
+        )
+      ),
+      
+      column(
+        width = 4,
+        wellPanel(
+          class = "middle-column",
+          h4("Phyloseq:"),
+          errorOutput("phyloseqMessage"),
+          verbatimTextOutput("phyloseqDetails")
+        )
+      ),
+      
+      column(
+        width = 4,
+        wellPanel(
+          class = "right-column",
+          tags$div(
+            tags$div(class = "pull-right",
+                     shiny_iconlink() %>%
+                       bs_embed_popover(title = "Project",
+                                        placement = "left",
+                                        html = "true",
+                                        content = tags$div(tags$p("Project is to group related data for storage."),
+                                                           tags$p("New Project button can help to create a new project title.")))),
+            h4("Project:")
+          ),
+          selectInput("project", NULL, NULL),
+          actionButton("newProjectButton", "New Project")
+        ),
+        
+        wellPanel(
+          class = "right-column",
+          h4("Save:"),
+          textInput("dataLabel", "Data Label") %>%
+            shinyInput_label_embed(
+              shiny_iconlink() %>%
+                bs_embed_popover(title = "Data Label",
+                                 placement = "left",
+                                 html = "true",
+                                 content = "Data label is to name the imported data. The data will be recalled by the data label within Genepiper")
+            ),
+          errorOutput("saveMessage"),
+          actionButton("saveButton", "Save")
+        ),
+        
+        wellPanel(
+          class = "right-column",
+          h4("Download Phyloseq RDS:"),
+          downloadButton("downloadPhyloseqButton")
+        )
+      )
+    )
+  )
+)
