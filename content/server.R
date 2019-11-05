817ced94db2c5392183e379370d676258c94747f
raytonghk <raytong@hku.hk>
2019-11-05 16:33:53 +0800

    Fix update problem

--- content/server.R
+++ content/server.R
@@ -6,7 +6,7 @@ shinyServer(
           input$updateButton,
           {
               system("git clone https://github.com/raytonghk/genepiper.git /srv/shiny-server/genepiper")
-              system("mv /srv/shiny-server/genepiper/* /srv/shiny-server")
+              system("cp -R /srv/shiny-server/genepiper/* /srv/shiny-server/")
               system("rm -rf /srv/shiny-server/genepiper")
           }
       )
