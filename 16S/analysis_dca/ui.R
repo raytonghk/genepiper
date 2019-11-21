096ae4afdf8efafc569e98fb7882188c7c9e244a
yukicyk <yukichanyk@gmail.com>
2019-11-21 18:26:20 +0800

    Update Description and References

--- 16S/analysis_dca/ui.R
+++ 16S/analysis_dca/ui.R
@@ -44,14 +44,24 @@ shinyUI(
         h4("Detrended Correspondence Analysis (DCA)"),
         tags$div(
           class = "desc",
-          p("Detrended correspondence analysis (DCA) is a multivariate statistical technique widely used by ecologists to find the main factors or gradients in large, species-rich but usually sparse data matrices that typify ecological community data. DCA is frequently used to suppress artifacts inherent in most other multivariate analyses when applied to gradient data."),
-          p("DCA is an iterative algorithm that has shown itself to be a highly reliable and useful tool for data exploration and summary in community ecology (Shaw 2003). It starts by running a standard ordination (CA or reciprocal averaging) on the data, to produce the initial horse-shoe curve in which the 1st ordination axis distorts into the 2nd axis. It then divides the first axis into segments (default = 26), and rescales each segment to have mean value of zero on the 2nd axis - this effectively squashes the curve flat. It also rescales the axis so that the ends are no longer compressed relative to the middle, so that 1 DCA unit approximates to the same rate of turnover all the way through the data: the rule of thumb is that 4 DCA units mean that there has been a total turnover in the community. Ter Braak and Prentice (1987, p. 122) warn against the non-linear rescaling of the axes due to robustness issues and recommend using detrending-by-polynomials only."),
-          a(target = "_blank", href="https://en.wikipedia.org/wiki/Detrended_correspondence_analysis", "From Wikipedia")
+          p("Detrended correspondence analysis (DCA) is an exploratory multivariate statistical technique developed to correct for three major problems of Correspondence Analysis (CA): (1) The arch effect, (2) compression at the ends of the gradient, and (3) rare species have an unduly large influence the analysis (Legendre and Gallagher 2001). Arch effect is a mathematical artefact where distribution of objects along the first canonical axis is also partially mirrored in the second axis, because CA axes are constrained to be uncorrelated but not necessarily unrelated of each other (Hill & Gauch 1980). Because of this problem, the second CA axis is an artifact and cannot easily be interpreted. Because of the second problem, the spacing of samples (and species) along the first axis is not necessarily related to the amount of change (or beta diversity) along the primary gradient. These problems are corrected in three phases: by Detrending, Rescaling and Downweighting."),
+          p("Detrending is the process of removing the arch effect. DCA does this by dividing the first axis into segments, and then by centering the second axis on zero (This process is much better described in Gauch 1982, Pielou 1984, Digby and Kempton 1987, and Kent and Coker 1992). More specifically, it starts by running a standard ordination (CA or reciprocal averaging) on the data, to produce the initial horse-shoe curve in which the 1st ordination axis distorts into the 2nd axis. It then divides the first axis into segments (default = 26), and rescales each segment to have mean value of zero on the 2nd axis - this effectively squashes the curve flat. It also rescales the axis so that the ends are no longer compressed relative to the middle, so that 1 DCA unit approximates to the same rate of turnover all the way through the data: the rule of thumb is that 4 DCA units mean that there has been a total turnover in the community. Species scores are taken as species optima. Species are scaled so that site scores are their direct weighted averages (alpha = 1). Note that while often useful, the detrending procedure does not always lead to an ordination output that better reflects the observed environmental gradients (see Legendre & Legendre 2012, for an in-depth discussion). DCA were shown to approximate well the ecological niche model and the typically expected Gaussian distribution of ecological variables (ter Braak & Looman 1986; ter Braak 1987).  A downside of DCA is that it can be sensitive to the number of segments used in the detrending algorithm (Jackson and Somers 1991)."),
+          a(target = "_blank", href="https://github.com/raytonghk/genepiper/wiki/15.-Detrended-Correspondence-Analysis", "see also our tutorial about DCA")
         ),
         tags$div(
           class = "ref",
-          h5("Hill, M.O. and Gauch, H.G. (1980). Detrended correspondence analysis: an improved ordination technique. Vegetatio 42, 47--58."),
-          h5("Oksanen, J. and Minchin, P.R. (1997). Instability of ordination results under changes in input data order: explanations and remedies. Journal of Vegetation Science 8, 447--454.")
+          h5("Digby PGN & Kempton RA (1987) Population and Community Biology Series: Multivariate Analysis of Ecological Communities. Chapman and Hall, London."),
+h5("Gauch HGJ (1982) Multivariate Analysis and Community Structure. Cambridge University Press, Cambridge, 298pp."),
+h5("Hill MO & Gauch HG (1980) Detrended correspondence analysis: an improved ordination technique. Vegetatio 42, 47--58."),
+h5("Jackson DA & Somers KM (1991) The ups and downs of detrended correspondence analysis. The American Naturalist 137:704-712."),
+h5("Kent M & Coker P (1992)  Vegetation description and analysis: a practical approach. Belhaven Press, London."),
+h5("Legendre P & Gallagher ED (2001) Ecologically meaningful transformations for ordination of species data. Oecologia 129:271-280."),
+h5("Legendre P & Legendre LFJ (2012) Numerical ecology. Vol. 24. Elsevier."),
+h5("Oksanen J & Minchin PR (1997) Instability of ordination results under changes in input data order: explanations and remedies. Journal of Vegetation Science 8, 447-454."),
+h5("Pielou EC (1984) The Interpretation of Ecological Data: A Primer on Classification and Ordination. Wiley, New York."),
+h5("ter Braak CJF & Prentice IC (1988) A theory of gradient analysis. Advances in Ecological Research, 18, 271–317."),
+h5("ter Braak CJF & Looman CWN (1986) Weighted averaging, logistic regression and the gaussian response model. Vegetatio, 65,3–11."),
+h5("ter Braak CJF (1987) Unimodal Models to Relate Species to Environment. Agricultural Mathematics Group, Wageningen.")
         )
       ),
       
@@ -132,50 +142,5 @@ shinyUI(
     )
   )
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
