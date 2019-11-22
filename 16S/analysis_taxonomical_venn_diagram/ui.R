f1308b56300fcfa0b3b60738f88b9dcfcd38be25
yukicyk <yukichanyk@gmail.com>
2019-11-22 14:38:28 +0800

    Update Description and References

--- 16S/analysis_taxonomical_venn_diagram/ui.R
+++ 16S/analysis_taxonomical_venn_diagram/ui.R
@@ -24,7 +24,20 @@ shinyUI(
     fluidRow(
       wellPanel(
         class = "desc-box",
-        h4("Taxonomical Venn Diagram")
+        h4("Taxonomical Venn Diagram"),
+        tags$div(
+          class = "desc",
+          p("This module provides options to plot a venn diagram comparing the membership of microbial communities at a user-specified taxonomic rank for <b>up to 5 samples/ groups</b>. Venn diagram is commonly used to viusalize the comparison of lists of objects.It is most often displayed as two or more circles in which each circle corresponds to a data set, and the overlap between the circles corresponds to the overlap between the data sets. User may then quickly observe the common and unique set of taxa amongst the samples/ groups of samples they are analyzing"),
+          p("GenePiper utilises the VennDiagram package (Chen & Boutros, 2011) to generate the plot. User may specify the taxonomic rank of taxa for the plot. From the Group column, user may select either 'Sample' or a varaible from the sample data table to group the samples for comparison"),
+          a(target = "_blank", href="https://github.com/raytonghk/genepiper/wiki/13.-Taxonomical-Venn-Diagram", "see our tutorial about this module.")          
+          ),
+        tags$div(
+          class = "ref",
+	h5("Chen H, Boutros PC (2011) VennDiagram: a package for the generation of highly-customizable Venn and Euler diagrams in R. BMC Bioinformatics 12, 35, doi:10.1186/1471-2105-12-35."),
+          h5("Ruskey F (2001) A Survey of Venn Diagrams. The Electronic Jornal of Combinatorics, DS#5, https://www.combinatorics.org/files/Surveys/ds5/ds5v2-2001/VennEJC.html"),
+          h5("McMurdie PJ, Holmes S (2013) phyloseq: An R Package for Reproducible Interactive Analysis and Graphics of Microbiome Census Data. PLoS ONE 8(4): e61217. https://doi.org/10.1371/journal.pone.0061217"),
+          h5("Sudarikov K, Tyakht A, Alexeev D (2017) Methods for the metagenomic data visualization and analysis. Curr Issues Mol Biol. 24:37-58.")
+        )
       ),
       
       column(
@@ -75,30 +88,6 @@ shinyUI(
     )
   )
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
